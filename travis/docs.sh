a3d09bba9cc25a75410ae8f33589d6ec2d442551
aerokube/selenoid-ui <aerokube@aerokube.github.com>
2017-05-08 02:35:54 +0300

    docs script fix - dont rewrite all

--- travis/docs.sh
+++ travis/docs.sh
@@ -3,14 +3,30 @@
 set -e
 TAGNAME=$1
 GH_REF=github.com/${TRAVIS_REPO_SLUG}
-
-cd docs ; mkdir -p output/${TAGNAME}; cp -R img output/${TAGNAME}/img
-
-docker run -v ${TRAVIS_BUILD_DIR}/docs/:/documents/ --name asciidoc-to-html asciidoctor/docker-asciidoctor asciidoctor -a revnumber=${TAGNAME} -D /documents/output/${TAGNAME} index.adoc
-
-cd output
-git init
 git config user.name "${TRAVIS_REPO_SLUG}"
 git config user.email "aerokube@aerokube.github.com"
-git add . ; git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+echo "Deleting old output"
+rm -rf ${TRAVIS_BUILD_DIR}/docs/output
+mkdir ${TRAVIS_BUILD_DIR}/docs/output
+git worktree prune
+rm -rf ${TRAVIS_BUILD_DIR}/.git/worktrees/docs/output/
+
+echo "Checking out gh-pages branch into docs/output"
+git worktree add -B gh-pages ${TRAVIS_BUILD_DIR}/docs/output origin/gh-pages
+
+echo "Removing existing files"
+mkdir -p ${TRAVIS_BUILD_DIR}/docs/output/${TAGNAME}
+rm -rf ${TRAVIS_BUILD_DIR}/docs/output/${TAGNAME}/*
+
+echo "Copying images"
+cp -R ${TRAVIS_BUILD_DIR}/docs/img ${TRAVIS_BUILD_DIR}/docs/output/${TAGNAME}/img
+echo "Generating docs"
+docker run -v ${TRAVIS_BUILD_DIR}/docs/:/documents/ --name asciidoc-to-html asciidoctor/docker-asciidoctor asciidoctor -a revnumber=${TAGNAME} -D /documents/output/${TAGNAME} index.adoc
+
+
+echo "Updating gh-pages branch"
+cd ${TRAVIS_BUILD_DIR}/docs/output && git add --all && git commit -m "Publishing to gh-pages"
+
+
+git push "https://${GITHUB_TOKEN}@${GH_REF}" HEAD:gh-pages
