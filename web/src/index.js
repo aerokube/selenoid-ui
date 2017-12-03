5e094432e3098a2b7fb06f8b46281c4278cbd193
Kirill Merkushev <lanwen@yandex.ru>
2017-12-03 21:20:31 +0300

    fix issue with more than 10 sessions (#93)
    Fixes #91, #92

--- web/src/index.js
+++ web/src/index.js
@@ -5,10 +5,6 @@ import Viewport from "containers/Viewport";
 
 import "static/dripicons.css"
 
-import { rxConnect } from "rx-connect";
-import rx5Adapter from "rx-connect/lib/rx5Adapter";
-rxConnect.adapter = rx5Adapter;
-
 ReactDOM.render(
     <Viewport/>,
     document.getElementById('root')
