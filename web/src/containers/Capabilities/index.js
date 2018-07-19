fc1cd9e75f04ae30589819fc4b051af15deac5ae
Alexander Andryashin <aandryashin@gmail.com>
2018-07-19 14:23:33 +0300

    Add go capabilities (#116)

--- web/src/containers/Capabilities/index.js
+++ web/src/containers/Capabilities/index.js
@@ -64,6 +64,15 @@ driver = Selenium::WebDriver.for(:remote,
   :url => "${origin}/wd/hub",
   :desired_capabilities => caps)
 `
+,
+	go: `// "github.com/tebeka/selenium"
+caps := selenium.Capabilities{"browserName": "${browser}", "version": "${version}"}
+driver, err := selenium.NewRemote(caps, "${origin}/wd/hub")
+if err != nil {
+	panic("create selenium session: %v\n", err)
+}
+defer driver.Quit()
+`
     }
 };
 
