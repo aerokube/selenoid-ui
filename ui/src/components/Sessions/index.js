c5c4e428762653099ceb4b4a482f0b0bb393b708
Kirill Merkushev <lanwen@yandex.ru>
2019-09-15 21:45:46 +0200

    Table view for sessions (#213)
    * Table view for sessions
    
    * table
    
    * footer removed

--- ui/src/components/Sessions/index.js
+++ ui/src/components/Sessions/index.js
@@ -5,21 +5,30 @@ import { ajax } from "rxjs/ajax";
 import { StyledSessions } from "./style.css";
 import BeatLoader from "react-spinners/BeatLoader";
 
+import styled from "styled-components/macro";
+
 const Sessions = ({ sessions = {} }) => {
     const ids = Object.keys(sessions);
 
     return (
         <StyledSessions>
             <div className="section-title">Sessions</div>
-            <TransitionGroup className={`sessions__list sessions__list_count-${ids.length}`}>
+            <TransitionGroup className="sessions__list">
                 {ids.length &&
-                    ids.map(id => {
-                        return (
-                            <CSSTransition key={id} timeout={500} classNames="session-container_state" unmountOnExit>
-                                <Session id={id} session={sessions[id]} />
-                            </CSSTransition>
-                        );
-                    })}
+                    ids
+                        .sort(a => (sessions[a].caps.labels && sessions[a].caps.labels.manual ? -1 : 1)) // can be moved to golang actually
+                        .map(id => {
+                            return (
+                                <CSSTransition
+                                    key={id}
+                                    timeout={500}
+                                    classNames="session-container_state"
+                                    unmountOnExit
+                                >
+                                    <Session id={id} session={sessions[id]} />
+                                </CSSTransition>
+                            );
+                        })}
             </TransitionGroup>
             <CSSTransition
                 in={!ids.length}
@@ -37,7 +46,7 @@ const Sessions = ({ sessions = {} }) => {
     );
 };
 
-const Session = ({ id, session: { caps } }) => {
+const Session = ({ id, session: { quota, caps } }) => {
     const [deleting, onDeleting] = useState(false);
 
     const deleteSession = e => {
@@ -58,35 +67,72 @@ const Session = ({ id, session: { caps } }) => {
     };
 
     return (
-        <div className="session-container">
-            <Link className="session-link" to={deleting ? `#` : `/sessions/${id}`}>
+        <div className={`session ${caps.labels && caps.labels.manual && "session_manual"}`}>
+            <SessionId>
+                <span className="quota">{quota}</span> / <span className="id">{id.substring(0, 8)}</span>
+            </SessionId>
+            <Link className="identity" to={deleting ? `#` : `/sessions/${id}`}>
                 <div className="browser">
                     <span className="name">{caps.browserName}</span>
                     <span className="version">{caps.version}</span>
                 </div>
+
                 {caps.name && (
-                    <div className="capability capability__name" title={caps.name}>
+                    <div className="session-name" title={caps.name}>
                         {caps.name}
                     </div>
                 )}
-                <button disabled={deleting} className="capability capability__session-delete" onClick={deleteSession}>
-                    {deleting ? (
-                        <BeatLoader size={2} color={"#fff"} />
-                    ) : (
-                        <span title="Delete" className="icon dripicons-power" />
-                    )}
-                </button>
+            </Link>
 
-                {caps.enableVNC && (
-                    <div className="capability capability__with-vnc">
-                        <span title="With VNC" className="icon dripicons-device-desktop" />
-                        &nbsp;
-                        <sup>VNC</sup>
+            <Capabilities>
+                {caps.labels && caps.labels.manual && <span className="capability capability__manual">Manual</span>}
+                {caps.enableVNC && <span className="capability">VNC</span>}
+                {caps.screenResolution && (
+                    <span className="capability  capability__resolution">{caps.screenResolution}</span>
+                )}
+            </Capabilities>
+            <Actions>
+                {caps.labels && caps.labels.manual && (
+                    <div className="capability capability__session-delete" onClick={deleteSession}>
+                        {deleting ? (
+                            <BeatLoader size={2} color={"#fff"} />
+                        ) : (
+                            <span title="Delete" className="icon dripicons-trash" />
+                        )}
                     </div>
                 )}
-            </Link>
+            </Actions>
         </div>
     );
 };
+
+const secondaryColor = "#aaa";
+
+const SessionId = styled.div`
+    display: flex;
+    align-items: center;
+    flex-basis: 140px;
+    padding-right: 5px;
+
+    .quota {
+        color: ${secondaryColor};
+        margin-right: 3px;
+    }
+
+    .id {
+        margin-left: 3px;
+    }
+`;
+
+const Capabilities = styled.div`
+    display: flex;
+    align-items: center;
+    flex: 1;
+`;
+
+const Actions = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
 export default Sessions;
