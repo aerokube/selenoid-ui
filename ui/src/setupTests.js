ca52c0d51aa45ddaf2836522169790312bcc9bf9
Kirill Merkushev <lanwen@yandex.ru>
2019-10-03 23:25:41 +0200

    update react to 16.10.1

--- ui/src/setupTests.js
+++ ui/src/setupTests.js
@@ -1,14 +1,5 @@
-import fs from "fs";
-import path from "path";
-
-// https://github.com/jsdom/jsdom/issues/639#issuecomment-371278152
-const mo = fs.readFileSync(path.resolve("node_modules", "mutationobserver-shim", "dist", "mutationobserver.min.js"), {
-    encoding: "utf-8",
-});
-const moScript = window.document.createElement("script");
-moScript.textContent = mo;
-
-window.document.body.appendChild(moScript);
+// ReferenceError: MutationObserver is not defined
+import "mutationobserver-shim";
 
 // https://github.com/gcedo/eventsourcemock
 import EventSource from "eventsourcemock";
