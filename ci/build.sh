93ebe2e7bbb52238016b1b62a798db0498e6b472
Ivan Krutov <vania-pooh@aerokube.com>
2022-06-20 11:17:39 +0300

    Bulding binary for ARM64

--- ci/build.sh
+++ ci/build.sh
@@ -3,8 +3,8 @@
 set -x
 
 export GO111MODULE="on"
-go get -u github.com/mitchellh/gox # cross compile
-gox -os "linux darwin windows" -arch "amd64" -osarch="windows/386" -output "dist/{{.Dir}}_{{.OS}}_{{.Arch}}" -ldflags "-X main.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.gitRevision=`git describe --tags || git rev-parse HEAD`"
+go install github.com/mitchellh/gox@latest # cross compile
+gox -os "linux darwin windows" -arch "amd64" -osarch="darwin/arm64" -osarch="windows/386" -output "dist/{{.Dir}}_{{.OS}}_{{.Arch}}" -ldflags "-X main.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.gitRevision=`git describe --tags || git rev-parse HEAD`"
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X main.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.gitRevision=`git describe --tags || git rev-parse HEAD`" # for docker
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o health-check healthcheck/main.go
 
