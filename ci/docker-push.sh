434ee9e7c1bbfa32ab12a8da6db74ba8a36eab79
Ivan Krutov <vania-pooh@aerokube.com>
2023-06-15 12:09:32 +0300

    Using regular Dockerfile instead of quay

--- ci/docker-push.sh
+++ ci/docker-push.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -f quay/Dockerfile -t quay.io/$GITHUB_REPOSITORY -t quay.io/$GITHUB_REPOSITORY:$1 .
+docker build -f Dockerfile -t quay.io/$GITHUB_REPOSITORY -t quay.io/$GITHUB_REPOSITORY:$1 .
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker push quay.io/$GITHUB_REPOSITORY
 docker push quay.io/$GITHUB_REPOSITORY:$1
